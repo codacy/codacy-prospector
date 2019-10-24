00cfc752387a9255239efdf338b2eb75e2d249e4
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-10-24 16:27:59 +0200

    feature: Remove plugins-test since no patterns are defined

--- docs/tests/example.py
+++
@@ -1,8 +0,0 @@
-##Patterns: pylint_unused-variable
-import json
-def readJsonFile(path):
-    ##Err: pylint_unused-variable
-    a = 1
-    with open(path, 'r') as file:
-        res = json.loads(file.read())
-    return res
