474e110f767c5c04ce9c22aa7d5203a6e033653b
DMarinhoCodacy <david.marinho@codacy.com>
2026-04-07 14:47:13 +0100

    Fix issue with configuration files

--- src/codacy_prospector.py
+++ src/codacy_prospector.py
@@ -98,7 +98,7 @@ def readConfiguration(configFile, srcDir):
         configuration = readJsonFile(configFile)
         files = configuration.get('files') or allFiles()
         tools = [t for t in configuration['tools'] if t['name'] == 'prospector']
-        if tools and 'patterns' in tools[0]:
+        if tools and 'patterns' in tools[0] and len(tools[0]['patterns'])>0:
             prospector = tools[0]
             tools = set([p['patternId'] for p in prospector.get('patterns') or []])
             tools_to_disable = default_tools.difference(tools)
@@ -126,6 +126,7 @@ def runTool(configFile, srcDir):
     (options, files) = readConfiguration(configFile, srcDir)
     res = []
     filesWithPath = [os.path.join(srcDir,f) for f in files]
+    print(options)
     for chunk in chunks(filesWithPath, 10):
         res.extend(runProspectorWith(options, chunk, srcDir))
     for result in res:
